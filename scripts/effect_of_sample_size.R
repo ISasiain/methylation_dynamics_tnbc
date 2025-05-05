7a180a09058fbf50780662a8f72a82e507847168
ISasiain <inakisasiainca@gmail.com>
2025-05-05 14:49:25 +0200

    Update and create scripts

--- scripts/effect_of_sample_size.R
+++ scripts/effect_of_sample_size.R
@@ -102,160 +102,160 @@ for (sample_n in samples_n) {
 
 
 
-#
-# ANALYSE OUTPUT
-#
-
-# List files of interest
-my_files <- list.files("/Users/isasiain/PhD/Projects/project_3/analysis/cassettes_resampling/all_distal/")
-my_files_60 <- my_files[grepl("^n_100_beta_10_*", my_files)]
-my_files_100 <- my_files[grepl("^n_100_beta_10_*", my_files)]
-my_files_120 <- my_files[grepl("^n_100_beta_10_*", my_files)]
-
-# Lists of cassettes and samples used
-list_of_cassettes <- list()
-list_of_samples <- list()
-
-for (file in my_files_100) {
-  
-  # Appending cassettes 
-  list_of_cassettes[[file]] <- readRDS(paste0("/Users/isasiain/PhD/Projects/project_3/analysis/cassettes_resampling/all_distal/",
-                                              file))$colors
-  # Appending samples
-  list_of_samples[[file]] <- rownames(readRDS(paste0("/Users/isasiain/PhD/Projects/project_3/analysis/cassettes_resampling/all_distal/",
-                                                       file))$MEs)
-}
-
-# Comparing CpGs included after variance filtering
-all_cpgs <- unique(unlist(lapply(list_of_cassettes, names)))
-
-df_included_cpgs <- as.data.frame(matrix(ncol=length(my_files),
-                                         nrow=length(all_cpgs)))
-
-colnames(df_included_cpgs) <- my_files
-rownames(df_included_cpgs) <- all_cpgs
-
-# Counting the included CpGs
-for (file_name in colnames(df_included_cpgs)) {
-  
-  df_included_cpgs[,file_name] <- all_cpgs %in% names(list_of_cassettes[[file_name]]) 
-  
-}
-
-# Calculating number of kept CpGs in each resampling
-used_cpgs <- rowSums(df_included_cpgs)
-
-# Plotting hist + density
-hist(rowSums(df_included_cpgs),
-     freq = FALSE,
-     breaks = 20,
-     col = "lightblue",
-     border = "black",
-     xaxt = "n",  # suppress x-axis
-     main = NULL,
-     xlab = "Resamplings",
-     ylab="Fraction of CpGs")
-
-# Add your custom x-axis
-axis(side = 1, at = c(1, 5, 10, 15, 20))
-
-# Add the density line
-lines(density(rowSums(df_included_cpgs)), col = "red", lwd = 2)
-
-
-
-# # Plotting heatmaps
+# #
+# # ANALYSE OUTPUT
+# #
 # 
-# # Define the cassettes to include
-# selected_cassettes <- 1:7
-# file_names <- "n_60_beta_10_resampling_3.rds"
+# # List files of interest
+# my_files <- list.files("/Users/isasiain/PhD/Projects/project_3/analysis/cassettes_resampling/all_distal/")
+# my_files_60 <- my_files[grepl("^n_100_beta_10_*", my_files)]
+# my_files_100 <- my_files[grepl("^n_100_beta_10_*", my_files)]
+# my_files_120 <- my_files[grepl("^n_100_beta_10_*", my_files)]
 # 
-# # Extract CpGs belonging to each cassette
-# cpg_list <- lapply(selected_cassettes, function(cassette) {
-#   names(list_of_cassettes[[file_names]][list_of_cassettes[[file_names]] == cassette])
-# })
+# # Lists of cassettes and samples used
+# list_of_cassettes <- list()
+# list_of_samples <- list()
 # 
-# names(cpg_list) <- selected_cassettes  # Assign cassette names
+# for (file in my_files_100) {
+#   
+#   # Appending cassettes 
+#   list_of_cassettes[[file]] <- readRDS(paste0("/Users/isasiain/PhD/Projects/project_3/analysis/cassettes_resampling/all_distal/",
+#                                               file))$colors
+#   # Appending samples
+#   list_of_samples[[file]] <- rownames(readRDS(paste0("/Users/isasiain/PhD/Projects/project_3/analysis/cassettes_resampling/all_distal/",
+#                                                        file))$MEs)
+# }
 # 
-# # Flatten the list to get selected CpGs
-# selected_CpGs <- as.character(unlist(cpg_list))
+# # Comparing CpGs included after variance filtering
+# all_cpgs <- unique(unlist(lapply(list_of_cassettes, names)))
 # 
-# # Subset the adjusted beta values
-# beta_subset <- betaAdj[selected_CpGs, list_of_samples[[file_names]]]
+# df_included_cpgs <- as.data.frame(matrix(ncol=length(my_files),
+#                                          nrow=length(all_cpgs)))
 # 
-# # Create a cassette grouping factor
-# cassette_factor <- factor(list_of_cassettes[[file_names]][selected_CpGs], levels = selected_cassettes)
+# colnames(df_included_cpgs) <- my_files
+# rownames(df_included_cpgs) <- all_cpgs
 # 
-# # Compute CpG counts per cassette
-# cassette_counts <- sapply(cpg_list, length)
-# row_labels <- paste0(selected_cassettes)
+# # Counting the included CpGs
+# for (file_name in colnames(df_included_cpgs)) {
+#   
+#   df_included_cpgs[,file_name] <- all_cpgs %in% names(list_of_cassettes[[file_name]]) 
+#   
+# }
 # 
-# # Determine column annotation
-# col_annotation <- HeatmapAnnotation(df=data.frame("Epitype" = orig_epi[list_of_samples[[file_names]], "NMF_atacDistal"]),
-#                                     col=list("Epitype"=c("Basal1" = "tomato4", "Basal2" = "salmon2", "Basal3" = "red2", 
-#                                                 "nonBasal1" = "cadetblue1", "nonBasal2" = "dodgerblue")))
+# # Calculating number of kept CpGs in each resampling
+# used_cpgs <- rowSums(df_included_cpgs)
 # 
-# Heatmap(beta_subset, 
-#         cluster_rows = FALSE, 
-#         cluster_columns = TRUE, 
-#         show_row_names = FALSE, 
-#         show_column_names = FALSE, 
-#         row_split = cassette_factor, 
-#         row_title = row_labels,  
-#         column_title = paste("CpG Methylation Heatmap by Cassettes (Beta =", beta, ")"),
-#         top_annotation = col_annotation,
-#         use_raster = FALSE)
-
-
-# SAVING CASSETTE ASSIGNMENTS TO CSV
-
-list_of_cassettes <- list()
-
-# Getting cassetttes for n_resapmlings of interest
-for (file in my_files_100) {
-  
-  # Appending cassettes 
-  list_of_cassettes[[file]] <- readRDS(paste0("/Users/isasiain/PhD/Projects/project_3/analysis/cassettes_resampling/all_distal/",
-                                              file))$colors
-}
-
-# Getting all CpGs used in every resampling
-all_cpgs <- unique(unlist(lapply(list_of_cassettes, names)))
-
-# Generating df to store the data
-df_of_cassette_assignments <- as.data.frame(matrix(NA, nrow = length(all_cpgs), ncol = length(list_of_cassettes)))
-rownames(df_of_cassette_assignments) <- all_cpgs
-
-# Populating dataframe
-for (n_col in seq_along(list_of_cassettes)) {
-  cassette <- list_of_cassettes[[n_col]]
-  common_cpgs <- intersect(all_cpgs, names(cassette))
-  df_of_cassette_assignments[common_cpgs, n_col] <- cassette[common_cpgs]
-}
-
-# Saving file as csv
-write.csv(df_of_cassette_assignments,
-          "/Users/isasiain/PhD/Projects/project_3/analysis/cassettes_resampling/consensus/n_res100_cassettes.csv")
-
-
-### SAVING TOP VARIANCE 5000 CpGs FOR ANALYSIS IN PYTHON
-
-
-# Using only distal cpgs
-distal_cpgs <- annoObj$illuminaID[which(annoObj$featureClass == "distal" | annoObj$featureClass == "distal body")]
-
-filtered_betas <- betaAdj[rownames(betaAdj) %in% distal_cpgs, ]
-
-# Calculate variance
-variace_of_cpgs <- apply(filtered_betas, MARGIN = 1, FUN = var)
-
-# Getting first 5000 and 10000 cpgs based on variance
-top_var_5000 <- rownames(filtered_betas)[order(variace_of_cpgs)[1:5000]]
-top_var_10000 <- rownames(filtered_betas)[order(variace_of_cpgs)[1:10000]]
-
-# Saving vectors as txt files
-write.table(top_var_5000, file = "/Users/isasiain/PhD/Projects/project_3/analysis/cassettes_resampling/consensus/distal_top_var_5000.txt", 
-            row.names = FALSE, col.names = FALSE, quote = FALSE)
-
-write.table(top_var_10000, file = "/Users/isasiain/PhD/Projects/project_3/analysis/cassettes_resampling/consensus/distal_top_var_10000.txt", 
-            row.names = FALSE, col.names = FALSE, quote = FALSE)
+# # Plotting hist + density
+# hist(rowSums(df_included_cpgs),
+#      freq = FALSE,
+#      breaks = 20,
+#      col = "lightblue",
+#      border = "black",
+#      xaxt = "n",  # suppress x-axis
+#      main = NULL,
+#      xlab = "Resamplings",
+#      ylab="Fraction of CpGs")
+# 
+# # Add your custom x-axis
+# axis(side = 1, at = c(1, 5, 10, 15, 20))
+# 
+# # Add the density line
+# lines(density(rowSums(df_included_cpgs)), col = "red", lwd = 2)
+# 
+# 
+# 
+# # # Plotting heatmaps
+# # 
+# # # Define the cassettes to include
+# # selected_cassettes <- 1:7
+# # file_names <- "n_60_beta_10_resampling_3.rds"
+# # 
+# # # Extract CpGs belonging to each cassette
+# # cpg_list <- lapply(selected_cassettes, function(cassette) {
+# #   names(list_of_cassettes[[file_names]][list_of_cassettes[[file_names]] == cassette])
+# # })
+# # 
+# # names(cpg_list) <- selected_cassettes  # Assign cassette names
+# # 
+# # # Flatten the list to get selected CpGs
+# # selected_CpGs <- as.character(unlist(cpg_list))
+# # 
+# # # Subset the adjusted beta values
+# # beta_subset <- betaAdj[selected_CpGs, list_of_samples[[file_names]]]
+# # 
+# # # Create a cassette grouping factor
+# # cassette_factor <- factor(list_of_cassettes[[file_names]][selected_CpGs], levels = selected_cassettes)
+# # 
+# # # Compute CpG counts per cassette
+# # cassette_counts <- sapply(cpg_list, length)
+# # row_labels <- paste0(selected_cassettes)
+# # 
+# # # Determine column annotation
+# # col_annotation <- HeatmapAnnotation(df=data.frame("Epitype" = orig_epi[list_of_samples[[file_names]], "NMF_atacDistal"]),
+# #                                     col=list("Epitype"=c("Basal1" = "tomato4", "Basal2" = "salmon2", "Basal3" = "red2", 
+# #                                                 "nonBasal1" = "cadetblue1", "nonBasal2" = "dodgerblue")))
+# # 
+# # Heatmap(beta_subset, 
+# #         cluster_rows = FALSE, 
+# #         cluster_columns = TRUE, 
+# #         show_row_names = FALSE, 
+# #         show_column_names = FALSE, 
+# #         row_split = cassette_factor, 
+# #         row_title = row_labels,  
+# #         column_title = paste("CpG Methylation Heatmap by Cassettes (Beta =", beta, ")"),
+# #         top_annotation = col_annotation,
+# #         use_raster = FALSE)
+# 
+# 
+# # SAVING CASSETTE ASSIGNMENTS TO CSV
+# 
+# list_of_cassettes <- list()
+# 
+# # Getting cassetttes for n_resapmlings of interest
+# for (file in my_files_100) {
+#   
+#   # Appending cassettes 
+#   list_of_cassettes[[file]] <- readRDS(paste0("/Users/isasiain/PhD/Projects/project_3/analysis/cassettes_resampling/all_distal/",
+#                                               file))$colors
+# }
+# 
+# # Getting all CpGs used in every resampling
+# all_cpgs <- unique(unlist(lapply(list_of_cassettes, names)))
+# 
+# # Generating df to store the data
+# df_of_cassette_assignments <- as.data.frame(matrix(NA, nrow = length(all_cpgs), ncol = length(list_of_cassettes)))
+# rownames(df_of_cassette_assignments) <- all_cpgs
+# 
+# # Populating dataframe
+# for (n_col in seq_along(list_of_cassettes)) {
+#   cassette <- list_of_cassettes[[n_col]]
+#   common_cpgs <- intersect(all_cpgs, names(cassette))
+#   df_of_cassette_assignments[common_cpgs, n_col] <- cassette[common_cpgs]
+# }
+# 
+# # Saving file as csv
+# write.csv(df_of_cassette_assignments,
+#           "/Users/isasiain/PhD/Projects/project_3/analysis/cassettes_resampling/consensus/n_res100_cassettes.csv")
+# 
+# 
+# ### SAVING TOP VARIANCE 5000 CpGs FOR ANALYSIS IN PYTHON
+# 
+# 
+# # Using only distal cpgs
+# distal_cpgs <- annoObj$illuminaID[which(annoObj$featureClass == "distal" | annoObj$featureClass == "distal body")]
+# 
+# filtered_betas <- betaAdj[rownames(betaAdj) %in% distal_cpgs, ]
+# 
+# # Calculate variance
+# variace_of_cpgs <- apply(filtered_betas, MARGIN = 1, FUN = var)
+# 
+# # Getting first 5000 and 10000 cpgs based on variance
+# top_var_5000 <- rownames(filtered_betas)[order(variace_of_cpgs)[1:5000]]
+# top_var_10000 <- rownames(filtered_betas)[order(variace_of_cpgs)[1:10000]]
+# 
+# # Saving vectors as txt files
+# write.table(top_var_5000, file = "/Users/isasiain/PhD/Projects/project_3/analysis/cassettes_resampling/consensus/distal_top_var_5000.txt", 
+#             row.names = FALSE, col.names = FALSE, quote = FALSE)
+# 
+# write.table(top_var_10000, file = "/Users/isasiain/PhD/Projects/project_3/analysis/cassettes_resampling/consensus/distal_top_var_10000.txt", 
+#             row.names = FALSE, col.names = FALSE, quote = FALSE)
